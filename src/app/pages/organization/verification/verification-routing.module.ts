04619ce7cdbeec6d80cdd070f3945d7ba38a4a67
imorgann <imanuelnwachukwu17@gmail.com>
2023-03-10 14:32:14 +0100

    task(feature/organization-verification)done

--- src/app/pages/organization/verification/verification-routing.module.ts
+++ src/app/pages/organization/verification/verification-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchResultsComponent } from './start-verification/search-results/search-results.component';
+import { VerifyDocumentsComponent } from './start-verification/search-results/verify-documents/verify-documents.component';
+import { StartVerificationComponent } from './start-verification/start-verification.component';
+import { VerificationComponent } from './verification.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: VerificationComponent,},
+
+  {path: 'verify-documents', component: StartVerificationComponent},
+  {path: 'verify-documents/:id', component: SearchResultsComponent},
+  {path: 'verify-documents/:id/:id', component: VerifyDocumentsComponent},
+
+  {path: 'view-verified-documents/:id', component: VerifyDocumentsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
