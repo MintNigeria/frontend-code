cf844f02821e48793ba7b6520f6caf800c39630b
imorgann <imanuelnwachukwu17@gmail.com>
2023-03-10 10:33:44 +0100

    task(feature/organization-sidebar)done

--- src/app/pages/organization/organization-routing.module.ts
+++ src/app/pages/organization/organization-routing.module.ts
@@ -4,7 +4,33 @@ import { OrganizationComponent } from './organization.component';
 
 
 const routes: Routes = [
-  {path: '', component: OrganizationComponent}
+  {path: '', component: OrganizationComponent,children: [
+    {
+      path: 'dashboard',
+      loadChildren: () =>
+        import('../organization/dashboard/dashboard.module').then((m) => m.DashboardModule),
+    },
+    {
+      path: 'verifications',
+      loadChildren: () =>
+        import('../organization/verification/verification.module').then((m) => m.VerificationModule),
+    },
+    {
+      path: 'transactions',
+      loadChildren: () =>
+        import('../organization/transactions/transactions.module').then((m) => m.TransactionsModule),
+    },
+    {
+      path: 'value-added-services',
+      loadChildren: () =>
+        import('../organization/value-added-services/value-added-services.module').then((m) => m.ValueAddedServicesModule),
+    },
+    {
+      path: 'settings',
+      loadChildren: () =>
+        import('../organization/settings/settings.module').then((m) => m.SettingsModule),
+    },
+  ]}
 ];
 
 @NgModule({
---
+++ src/app/pages/organization/value-added-services/value-added-services.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-value-added-services',
+  templateUrl: './value-added-services.component.html',
+  styleUrls: ['./value-added-services.component.scss']
+})
+export class ValueAddedServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
