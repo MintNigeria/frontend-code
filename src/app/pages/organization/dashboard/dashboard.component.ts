8ea328ce3361a8beb93ac9af7a0ca94ef2c7d4d9
imorgann <imanuelnwachukwu17@gmail.com>
2023-03-13 17:33:03 +0100

    task(feature/organization-dashboard)done

--- src/app/pages/organization/dashboard/dashboard.component.ts
+++ src/app/pages/organization/dashboard/dashboard.component.ts
@@ -7,6 +7,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  verificationHistory = [
+  {
+    id: '1',
+    date: '12/01/2023',
+    verificationID: '#3066',
+    name:'Adekunle Ciroma',
+    phoneNumber: '0819036356377',
+    institution: 'University of Lagos',
+    department: 'Banking and Finance',
+    reasonForRequest: 'Educational Verification',
+    action: 'View'
+  },
+  {
+    id: '2',
+    date: '12/01/2023',
+    verificationID: '#3066',
+    name:'Adekunle Ciroma',
+    phoneNumber: '0819036356377',
+    institution: 'University of Lagos',
+    department: 'Banking and Finance',
+    reasonForRequest: 'Educational Verification',
+    action: 'View'
+  },
+  {
+    id: '3',
+    date: '12/01/2023',
+    verificationID: '#3066',
+    name:'Adekunle Ciroma',
+    phoneNumber: '0819036356377',
+    institution: 'University of Lagos',
+    department: 'Banking and Finance',
+    reasonForRequest: 'Educational Verification',
+    action: 'View'
+  },
+  {
+    id: '4',
+    date: '12/01/2023',
+    verificationID: '#3066',
+    name:'Adekunle Ciroma',
+    phoneNumber: '0819036356377',
+    institution: 'University of Lagos',
+    department: 'Banking and Finance',
+    reasonForRequest: 'Educational Verification',
+    action: 'View'
+  },
+  {
+    id: '5',
+    date: '12/01/2023',
+    verificationID: '#3066',
+    name:'Adekunle Ciroma',
+    phoneNumber: '0819036356377',
+    institution: 'University of Lagos',
+    department: 'Banking and Finance',
+    reasonForRequest: 'Educational Verification',
+    action: 'View'
+  },
+  {
+    id: '6',
+    date: '12/01/2023',
+    verificationID: '#3066',
+    name:'Adekunle Ciroma',
+    phoneNumber: '0819036356377',
+    institution: 'University of Lagos',
+    department: 'Banking and Finance',
+    reasonForRequest: 'Educational Verification',
+    action: 'View'
+  }
+ ]
+
   constructor() { }
 
   ngOnInit(): void {
