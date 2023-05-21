696f359400439cc56c96c113325fd470783a4929
Alaketu Olamilekan Soliu <alaketuolamilekan02@gmail.com>
2023-05-21 22:15:02 +0100

    task(UAT): Improve auth load time by preloading auth with module strategy, mobile reponsiveness checks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth/auth.guards';
+import { ModuleLoadingStrategyService } from './module.loading.strategy';
 
 const routes: Routes = [
   {
@@ -12,12 +13,13 @@ const routes: Routes = [
     path: 'auth',
     loadChildren: () =>
       import('./pages/auth/auth.module').then((m) => m.AuthModule),
+      data: { preload: true }
+
   },
   // {
   //   path: 'authentication',
   //   loadChildren: () =>
   //     import('./pages/authentication/authentication.module').then((m) => m.AuthenticationModule),
-  //     data: { preload: true }
   // },
   {
     path: 'institution',
@@ -40,7 +42,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: ModuleLoadingStrategyService
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
