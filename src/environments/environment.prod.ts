4b8e30cfcd8f41d4f49b3feb541c7002995d0d8a
TemiLayonn <65893987+temilayonn@users.noreply.github.com>
2023-06-14 06:05:11 -0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   recaptchaKey: '6LfnQhgmAAAAAEgAA41biAAa2A_5ImpmAWJU-KwD',
-  apiBaseURI: 'http://certificatesverification.org:2008/',
+  //apiBaseURI: 'http://certificatesverification.org:2008/',
+  apiBaseURI: 'https://certificatesverification.org/'
   // apiBaseURI: 'http://102.164.32.78:2000/',
   // apiBaseURI: 'http://40.67.166.30:2000/',
   pkKey: 'pk_test_dc7c232fe6f9d7ff3eaeeda082074ef765244dbf',
