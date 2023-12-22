e69cf9140c5434673830fec674d653867d212cf0
Alaketu Olamilekan Soliu <alaketuolamilekan02@gmail.com>
2023-12-22 20:07:55 +0100

    task(env): clear cache and update recapcha key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  recaptchaKey: '6Le5tzkpAAAAAL3tSNuyENezcBugdUN8CqHhhtUL', //prod
+  recaptchaKey: '6LdZ5DkpAAAAAE19Yx1zdct6DOIJ0cLu4c3Ynfxg', //prod
   // recaptchaKey: '6LfnQhgmAAAAAEgAA41biAAa2A_5ImpmAWJU-KwD', // staging
   // recaptchaKey: '6LcLK9MnAAAAAIyZS-UebpSGm0PsGiQXsjMB36-x',
   //apiBaseURI: 'http://certificatesverification.org:2008/',
